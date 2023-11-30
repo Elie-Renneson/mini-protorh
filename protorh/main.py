7fde1282631393113f0319526d916b4b16924c3e
sherlock <sherlock@exterminator>
2023-11-30 09:50:10 +0000

    first commit

---
+++ protorh/main.py
@@ -0,0 +1,16 @@
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Adjust this to allow specific origins
+    allow_methods=["*"],  # Adjust this to allow specific methods (e.g., ["GET", "POST"])
+    allow_headers=["*"],  # Adjust this to allow specific headers
+)
+
+
+@app.get("/")
+async def hello():
+    return {"message": "Hello World!"}
